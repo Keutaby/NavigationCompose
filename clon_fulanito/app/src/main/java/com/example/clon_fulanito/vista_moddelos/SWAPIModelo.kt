beb0e3b9af6ea913568f0d9d73e7c3b40c9adce7
Keutaby <al217212@alumnos.uacj.mx>
2025-04-09 08:45:10 -0600

    [AGREGAR] cargando circular y boton anterior

--- clon_fulanito/app/src/main/java/com/example/clon_fulanito/vista_moddelos/SWAPIModelo.kt
+++ clon_fulanito/app/src/main/java/com/example/clon_fulanito/vista_moddelos/SWAPIModelo.kt
@@ -10,24 +10,39 @@ import com.example.clon_fulanito.API.SWAPI.RepositorioSWAPI
 import com.example.clon_fulanito.modelos.swapi.PaginaContenedora
 import kotlinx.coroutines.launch
 
+enum class Estados{
+    cargando,
+    mostrando_lista_de_naves,
+    mostrando_nave,
+    error,
+    registrar_frustracion
+}
+
 class SWAPIModelo: ViewModel(){
     private val repositorio = RepositorioSWAPI()
 
     private val _pagina_actual = MutableLiveData<PaginaContenedora>()
     val pagina_actual: LiveData<PaginaContenedora> = _pagina_actual
 
-    private var pagina_siguiente: String? = null
+    private val _estado_actual = MutableLiveData<Estados>(Estados.cargando)
+    val estado_actual: LiveData<Estados> = _estado_actual //Poner la variable en una vitrina
+
+    private val _mensaje = MutableLiveData("Por ahora nada")
+    //val mensaje: LiveData<String> = _mensaje
 
     fun descargar_pagina(pagina: Int = 1){
+        _estado_actual.postValue(Estados.cargando)
         Log.v("Pagina a descargar", "pagina: ${pagina}")
         viewModelScope.launch{
             try{
                 val pagina = repositorio.obtener_naves_espaciales(pagina)
 
                 _pagina_actual.postValue(pagina)
+                _estado_actual.postValue(Estados.mostrando_lista_de_naves)
             }
             catch(error: Exception){
                 Log.v("DESCARGA DE PAGINA SWAPI", "${error.message}")
+                _estado_actual.postValue(Estados.error)
             }
         }
     }
@@ -38,7 +53,6 @@ class SWAPIModelo: ViewModel(){
         Log.v("Cargando pagina", "pagina_siguiente: ${pagina_siguiente}")
         if(pagina_siguiente != null){
             descargar_pagina(pagina_siguiente)
-
         }
 
     }
@@ -46,9 +60,14 @@ class SWAPIModelo: ViewModel(){
     fun pasar_a_anterior_pagina() {
         val pagina_anterior: Int? = _pagina_actual.value?.indicar_pagina_anterior()
 
+        Log.v("Cargando pagina", "pagina siguiente: ${pagina_anterior}")
         if(pagina_anterior != null){
-            Log.v("Cargando pagina", "pagina siguiente: ${pagina_anterior}")
+            descargar_pagina(pagina_anterior)
         }
 
+    }
+
+    fun indicar_un_problema(){
+        _estado_actual.postValue(Estados.registrar_frustracion)
     }
 }
